3e4c223ddfabba5ce7c45175d735058a8b7b5ced
Sumaya04122006 <22501000039@lu.ac.ae>
2026-09-09 23:20:37 +0400

    Connect frontend to deployed API

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = https://liwa-achievement-tracker-api.onrender.com/git add Frontend/js/app.js;
 
 
 // ==========================================
